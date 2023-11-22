47b324b92cb9901a8b56a3792e1595a5b74e585e
Helge Spieker <helge@simula.no>
2023-11-22 22:55:06 +0100

    Add clustering + plot dendograms

--- script/dual_clustering.py
+++ script/dual_clustering.py
@@ -8,11 +8,13 @@ import pandas as pd
 import os.path as osp
 from scipy import stats
 from sklearn.neighbors import NearestNeighbors
-from common import split_data, load_data
-
-performances = [
-    "elapsedtime"  # we only consider this performance metric for the moment
-]
+from common import (
+    split_data,
+    load_data,
+    spearman_rank_distance,
+    pearson_correlation,
+    rank_difference_distance,
+)
 
 # The plan for this file is to have two clusterings:
 # 1. In the config feature space
@@ -29,7 +31,7 @@ performances = [
 # c) Performance Prediction
 # -
 
-
+# %%
 ## Configuration
 # Enter names of performance columns to consider
 performances = ["elapsedtime"]
@@ -69,19 +71,17 @@ average_ranks = rank_map.mean(axis=1)
 
 
 # This is a look up for performance measurements from inputname + configurationID
+all_input_names = pd.Series(perf_matrix.inputname.sort_values().unique())
+all_config_ids = pd.Series(perf_matrix.configurationID.sort_values().unique())
+
 input_config_map = (
-    perf_matrix[["inputname", "configurationID"] + performances]
+    data_split["train_data"][["inputname", "configurationID"] + performances]
     .sort_values(["inputname", "configurationID"])
     .set_index(["inputname", "configurationID"])
 )
-all_input_names = pd.Series(
-    input_config_map.index.get_level_values("inputname").unique()
-)
-all_config_ids = pd.Series(
-    input_config_map.index.get_level_values("configurationID").unique()
-)
+
 measurements = input_config_map.values.reshape(
-    (len(all_input_names), len(all_config_ids), len(performances))
+    (len(data_split["train_inp"]), len(data_split["train_cfg"]), len(performances))
 )
 len(all_input_names), len(all_config_ids), measurements.shape
 
@@ -89,10 +89,18 @@ len(all_input_names), len(all_config_ids), measurements.shape
 
 ### Rank-based distance matrix
 
-# distance_matrix = spearman_rank_distance(measurements)
+
 # Distance matrix may not include negative values, but spearman can be negative
-# model_dist = NearestNeighbors(metric="precomputed")
-# model_dist.fit(distance_matrix[:, :, 0])
+# distance_matrix = spearman_rank_distance(measurements)
+
+# Input x Input distance matrix
+distance_matrix_inputs = rank_difference_distance(measurements)
+distance_matrix_configs = rank_difference_distance(np.swapaxes(measurements, 0, 1))
+
+model_dist = NearestNeighbors(metric="precomputed")
+model_dist.fit(distance_matrix_inputs[:, :, 0])
+
+
 
 # %%
 ## Evaluation
@@ -111,3 +119,75 @@ def get_configuration_distances(measurements):
 
 def get_input_distances(measurements):
     return distance_matrix_by_first_axis(np.moveaxis(measurements, 0, 1))
+
+# %%
+reshaped_array = measurements.reshape(measurements.shape[0], -1)
+model = AgglomerativeClustering(n_clusters=10, compute_distances=True, linkage='average')
+model.fit(reshaped_array)
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+plot_dendrogram(model, truncate_mode="level", p=20, color_threshold=0.01)
+# %%
+# reshaped_array = measurements.reshape(measurements.shape[0], -1)
+model = AgglomerativeClustering(n_clusters=10, compute_distances=True, linkage='average', metric="precomputed")
+model.fit(distance_matrix_inputs[:, :, 0])
+
+
+plot_dendrogram(model, truncate_mode="level", p=20, color_threshold=0.01)
+# %%
+
+# TODO Implement wilcoxon or kendalltau as the distance between the two rankings
+
+def wilcoxon_distance(measurements):
+    # Vectorized wilcoxon with multiple measurements
+
+    # Breaks ties correctly by assigning same ranks, but potentially instable
+    # TODO Should work correctly if we drop `frames` which has constant value
+    # ranks = stats.rankdata(measurements, axis=1, method="min")
+
+    # Makes individual ranks
+    ranks = np.argsort(measurements, axis=1)
+
+    # The ranks array is 3D (A, B, C), and we need to expand it to 4D for pairwise comparison in A,
+    # while keeping C
+    expanded_rank_X_3d = ranks[:, np.newaxis, :, :]  # Expanding for A dimension
+    expanded_rank_Y_3d = ranks[np.newaxis, :, :, :]  # Expanding for A dimension
+
+    A = ranks.shape[0]
+    C = ranks.shape[2]
+
+    # Initialize the Spearman correlation matrix for each C
+    spearman_correlation_matrix_3d = np.empty((A, A, C))
+
+    # Calculate Spearman correlation matrix for each C
+    for c in range(C):
+        print(expanded_rank_X_3d[:, :, :, c].shape, expanded_rank_Y_3d[:, :, :, c].shape)
+        spearman_correlation_matrix_3d[:, :, c] = stats.kendalltau(
+            expanded_rank_X_3d[:, :, :, c], expanded_rank_Y_3d[:, :, :, c]
+        ).statistic
+
+    return spearman_correlation_matrix_3d
+
+dmi = wilcoxon_distance(measurements)
+# %%
